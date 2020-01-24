8c5fae1b4f07c1ad0dc6404359ab9fa2097de0d6
shiawaseli <shiawaseli@users.noreply.github.com>
2020-01-24 23:40:32 +0800

    C++ 句柄类2
    1、实现引用计数类，独立于目标对象
    2、使用引用计数类实现句柄类

--- C++/C++_day12/handle.h
+++ C++/C++_day12/handle.h
@@ -57,6 +57,7 @@ namespace lb
 		PointHandle(const PointHandle & ph) : m_up(ph.m_up) { ++(m_up->m_cnt); }
 		~PointHandle() { if (--(m_up->m_cnt) == 0) delete m_up; }
 		PointHandle& operator=(const PointHandle & ph);
+		friend std::ostream& operator<<(std::ostream & os, const PointHandle & ph);
 		int x() const { return m_up->x(); }
 		int y() const { return m_up->y(); }
 		int count() const { return m_up->count(); }
@@ -69,7 +70,7 @@ namespace lb
 		PointHandle& y(const int y);
 
 		bool checkCount();
-		void show() { std::cout << '(' << x() << ',' << y() << ')' << std::endl; }
+		void show() { std::cout << *this << std::endl; }
 	};
 	PointHandle& PointHandle::operator=(const PointHandle & ph)
 	{
@@ -78,6 +79,11 @@ namespace lb
 			delete m_up;
 		m_up = ph.m_up;
 		return *this;
+	}
+	std::ostream& operator<<(std::ostream & os, const PointHandle & ph)
+	{
+		os << '(' << ph.x() << ',' << ph.y() << ')';
+		return os;
 	}
 	PointHandle& PointHandle::x(const int x)
 	{
@@ -99,5 +105,89 @@ namespace lb
 			m_up = new UPoint(*m_up);
 		}
 		return true;
+	}
+
+	// 计数类,用于需要引用计数的类
+	class UseCount
+	{
+	private:
+		int *m_cnt;
+	public:
+		UseCount() : m_cnt(new int(1)) { }
+		UseCount(const UseCount & uc) : m_cnt(uc.m_cnt) { ++(*m_cnt); }
+		~UseCount() { if (--(*m_cnt) == 0) delete m_cnt; }
+		int count() const { return *m_cnt; }
+		bool only() const { return *m_cnt == 1; }
+		bool reattach(const UseCount & uc);
+		bool makeOnly();
+	private:
+		UseCount& operator=(const UseCount & uc) { } // 不实现,阻止直接赋值操作
+	};
+	bool UseCount::reattach(const UseCount & uc)
+	{
+		++(*uc.m_cnt);
+		if (--(*m_cnt) == 0)
+		{
+			delete m_cnt;
+			m_cnt = uc.m_cnt;
+			return true;
+		}
+		m_cnt = uc.m_cnt;
+		return false;
+	}
+	bool UseCount::makeOnly()
+	{
+		if (*m_cnt == 1)
+			return false;
+		--(*m_cnt);
+		m_cnt = new int(1);
+		return true;
+	}
+	class PointHandle2
+	{
+	private:
+		Point *m_p;
+		//int *m_cnt; // 也可以使用指针,但不如计数类方便
+		UseCount m_uc;
+	public:
+		PointHandle2() : m_p(new Point){ }
+		PointHandle2(const Point & p) : m_p(new Point(p)){ }
+		PointHandle2(const int x, const int y) : m_p(new Point(x, y)) { }
+		PointHandle2(const PointHandle2 & hp) : m_p(hp.m_p), m_uc(hp.m_uc) { }
+		~PointHandle2() { if (m_uc.only()) delete m_p; }
+		PointHandle2& operator=(const PointHandle2 & hp);
+		friend std::ostream& operator<<(std::ostream & os, const PointHandle2 & ph);
+		int x() const { return m_p->x(); }
+		int y() const { return m_p->y(); }
+		int count() const { return m_uc.count(); }
+		PointHandle2& x(const int x);
+		PointHandle2& y(const int y);
+		void show() { std::cout << *this << std::endl; }
+	};
+	PointHandle2& PointHandle2::operator=(const PointHandle2 & hp)
+	{
+		if (m_uc.reattach(hp.m_uc))
+			delete m_p;
+		m_p = hp.m_p;
+		return *this;
+	}
+	std::ostream& operator<<(std::ostream & os, const PointHandle2 & ph)
+	{
+		os << '(' << ph.x() << ',' << ph.y() << ')';
+		return os;
+	}
+	PointHandle2& PointHandle2::x(const int x)
+	{
+		if (m_uc.makeOnly())
+			m_p = new Point(*m_p);
+		m_p->x(x);
+		return *this;
+	}
+	PointHandle2& PointHandle2::y(const int y)
+	{
+		if (m_uc.makeOnly())
+			m_p = new Point(*m_p);
+		m_p->y(y);
+		return *this;
 	}
 }
--- C++/C++_day12/handle_test.cpp
+++ C++/C++_day12/handle_test.cpp
@@ -1,13 +1,12 @@
-#include "handle_1.h"
+#include "handle.h"
 #include <iostream>
 using namespace std;
 using namespace lb;
 
-int main()
+void test1()
 {
 	PointHandle ph;
-	cout << ph.count() << " ";
-	ph.show();
+	cout << ph.count() << " " << ph << endl;
 	ph.x(25);
 	ph.y(52);
 
@@ -22,6 +21,30 @@ int main()
 	ph4 = ph5;
 	ph4.x(100); // 写时拷贝,修改时进行拷贝
 	//ph4 = PointHandle(100, ph5.y()); // 作用类似于指针语义时的写时拷贝
+}
 
+void test2()
+{
+	PointHandle2 ph;
+	cout << ph.count() << " " << ph << endl;
+	ph.x(25);
+	ph.y(52);
+
+	PointHandle2 ph2 = ph, ph3(ph2);
+	PointHandle2 ph4(33, 6);
+	ph3 = ph4;
+	cout << ph.count() << " ";
+	ph.show();
+
+	Point p1(89, 67);
+	PointHandle2 ph5(p1);
+	ph4 = ph5;
+	ph4.x(100);
+}
+
+int main()
+{
+	test1();
+	test2();
 	return 0;
 }
